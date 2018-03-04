d5bf597d7a7806724645d1d1b9f1e2817d385eb7
itsnhduc <nhduc.lhp@gmail.com>
2018-03-04 14:14:09 +0800

    Make phone screen only scrollable when full

--- Assets/Scripts/ItemList.cs
+++ Assets/Scripts/ItemList.cs
@@ -8,12 +8,15 @@ public class ItemList : Singleton<ItemList>
 {
     public GameObject listItemPrefab;
     public float speed;
+    public int minScrollNumber;
 
     Rigidbody2D rb { get { return GetComponent<Rigidbody2D>(); } }
 
     public Vector2 MoveDirection { set { rb.velocity = value * speed; } }
 
     private Vector2 _originalLocalPos;
+
+    private bool _isScrollable = false;
 
     void Start()
     {
@@ -22,15 +25,18 @@ public class ItemList : Singleton<ItemList>
 
     void Update()
     {
-        bool upKey = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
-        bool downKey = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
-        if (upKey || downKey) MoveDirection = upKey ? Vector2.up : Vector2.down;
-		else MoveDirection = Vector2.zero;
-        if (transform.localPosition.y < _originalLocalPos.y)
-		{
-			MoveDirection = Vector2.zero;
-			transform.localPosition = _originalLocalPos;
-		}
+        if (_isScrollable)
+        {
+            bool upKey = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
+            bool downKey = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
+            if (upKey || downKey) MoveDirection = upKey ? Vector2.up : Vector2.down;
+            else MoveDirection = Vector2.zero;
+            if (transform.localPosition.y < _originalLocalPos.y)
+            {
+                MoveDirection = Vector2.zero;
+                transform.localPosition = _originalLocalPos;
+            }
+        }
     }
 
     public void Refresh()
@@ -48,7 +54,7 @@ public class ItemList : Singleton<ItemList>
     }
     private void _Render()
     {
-        transform.localPosition = _originalLocalPos;
+        // transform.localPosition = _originalLocalPos;
         var guyItems = GuyMovement.Instance.GetComponent<ItemStorage>().items.Select(p => p.productName);
         var remainingItems = Spawner.Instance.products.Where(p => p.Spawned).Select(p => p.productName);
         var guyItemInfo = guyItems.Select(p => new KeyValuePair<string, bool>(p, true));
@@ -63,5 +69,6 @@ public class ItemList : Singleton<ItemList>
             li.IsChecked = totalItemInfo.ElementAt(i).Value;
         }
         ItemCount.Instance.Count = totalItemInfo.Count();
+        _isScrollable = totalItemInfo.Count() >= minScrollNumber;
     }
 }
