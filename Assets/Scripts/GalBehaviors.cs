52b13e6e5b3652d6d40924c98aff2c7a50e70b09
itsnhduc <nhduc.lhp@gmail.com>
2018-03-04 14:48:08 +0800

    Add gal happy shaking animation

--- Assets/Scripts/GalBehaviors.cs
+++ Assets/Scripts/GalBehaviors.cs
@@ -7,34 +7,50 @@ using UnityEngine;
 public class GalBehaviors : Singleton<GalBehaviors>
 {
     public Sprite happySprite;
-	public float happyDuration;
+    public float happyDuration;
+    public float shakeAngle;
+    public float shakeInterval;
 
     SpriteRenderer sr { get { return GetComponent<SpriteRenderer>(); } }
 
-	private Sprite _originalSprite;
+    private Sprite _originalSprite;
 
     bool IsHappy
-	{
-		set
-		{
-			sr.sprite = value ? happySprite : _originalSprite;
-		}
-	}
+    {
+        get { return sr.sprite == happySprite; }
+        set
+        {
+            sr.sprite = value ? happySprite : _originalSprite;
+            if (value) StartCoroutine(Shake());
+        }
+    }
 
-	void Start()
-	{
-		_originalSprite = sr.sprite;
-	}
+    void Start()
+    {
+        _originalSprite = sr.sprite;
+    }
 
-	public void BecomeHappy()
-	{
-		StartCoroutine(Happy());
-	}
+    public void BecomeHappy()
+    {
+        StartCoroutine(Happy());
+    }
 
-	IEnumerator Happy()
-	{
-		IsHappy = true;
-		yield return new WaitForSeconds(happyDuration);
-		IsHappy = false;
-	}
+    IEnumerator Happy()
+    {
+        IsHappy = true;
+        yield return new WaitForSeconds(happyDuration);
+        IsHappy = false;
+    }
+
+    IEnumerator Shake()
+    {
+        int sign = 1;
+        while (IsHappy)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, sign * shakeAngle);
+            sign *= -1;
+            yield return new WaitForSeconds(shakeInterval);
+        }
+		transform.rotation = new Quaternion();
+    }
 }
