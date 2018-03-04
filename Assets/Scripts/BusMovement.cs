34a6ca695532d047ed4260e13dbc033cb39e0e4e
itsnhduc <nhduc.lhp@gmail.com>
2018-03-04 15:45:54 +0800

    Animate bus station

---
+++ Assets/Scripts/BusMovement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusMovement : MonoBehaviour
+{
+    public float offsetX;
+    public float offsetY;
+    public float minSpeed;
+    public float maxSpeed;
+	public float goRightPercentage;
+	public Sprite goLeftSprite;
+
+    Rigidbody2D rb { get { return GetComponent<Rigidbody2D>(); } }
+    SpriteRenderer sr { get { return GetComponent<SpriteRenderer>(); } }
+
+    void Start()
+    {
+		bool isGoingRight = UnityEngine.Random.Range(0, 1f) <= goRightPercentage / 100 ? true : false;
+		int zIndex = UnityEngine.Random.Range(-1, 2);
+		int sign = isGoingRight ? -1 : 1;
+		float speed = UnityEngine.Random.Range(minSpeed, maxSpeed);
+
+		if (!isGoingRight) sr.sprite = goLeftSprite;
+		sr.sortingOrder = zIndex;
+		transform.localPosition = new Vector2(sign * offsetX, transform.localPosition.y + offsetY * zIndex);
+		rb.velocity = -sign * Vector2.right * speed;
+    }
+
+	void Update()
+	{
+		if (Mathf.Abs(transform.localPosition.x) > offsetX) Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/BusStation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusStation : MonoBehaviour
+{
+	public GameObject busPrefab;
+	public float spawnDurationMin;
+	public float spawnDurationMax;
+
+	void Start()
+	{
+		StartCoroutine(SpawnBuses());
+	}
+
+	IEnumerator SpawnBuses()
+	{
+		while (true)
+		{
+			Instantiate(busPrefab, transform);
+			float duration = UnityEngine.Random.Range(spawnDurationMin, spawnDurationMax);
+			yield return new WaitForSeconds(duration);
+		}
+	}
+}
