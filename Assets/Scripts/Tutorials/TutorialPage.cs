843b38267c03706c90ecf53c0d17412fc587ca92
itsnhduc <nhduc.lhp@gmail.com>
2018-03-06 16:50:17 +0800

    Add tutorial scene

---
+++ Assets/Scripts/Tutorials/TutorialPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialPage : MonoBehaviour
+{
+	void Update()
+	{
+		bool useKey = Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Return);
+		if (useKey) SceneManager.LoadScene("Game");
+	}
+}
